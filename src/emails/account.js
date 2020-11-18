459042b8707f2b4d0cea86042326c962520c7d11
agsdtm7 <marsono.a2@gmail.com>
2020-11-18 11:48:13 -0500

    Fix the unhandledPromiseRejections

--- src/emails/account.js
+++ src/emails/account.js
@@ -8,6 +8,10 @@ const sendWelcomeEmail = (email, name) => {
         subject: 'Thanks for joining us',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
         html: '<strong>You can create your tasks and set a reminder alarm.</strong>'
+    }).then(() => {
+        console.log('A new user signed up, a welcome email sent successfully.')
+    }).catch((e) => {
+        console.log(e.response.body)
     })
 }
 
@@ -17,6 +21,10 @@ const sendCancellationEmail = (email, name) => {
         from: 'taskapp@moodle.portfolios.dx.am',
         subject: 'We are sorry to see you go',
         text: `Hi ${name}, We are sorry to see you go but we appreciate it if you let us know how we can do better.`
+    }).then(() => {
+        console.log('A user dropped out! A farewell email sent ')
+    }).catch((e) => {
+        console.log(e.response.body)
     })
 }
 
